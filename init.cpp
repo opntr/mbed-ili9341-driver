f5ae7d1918d8a089f8362be8f58ffed25e76b4a2
Oliver Pinter <oliver.pntr@gmail.com>
2020-08-05 21:37:08 +0200

    implement setup_fscm function
    NOT YET TESTED
    
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

--- init.cpp
+++ init.cpp
@@ -56,8 +56,64 @@ ili9341::setup_ctrl_lines()
 void
 ili9341::setup_fscm()
 {
-#if 0
-#endif /* 0 */
+    FSMC_NORSRAM_InitTypeDef FSMC_NORSRAMInitStructure;
+    FSMC_NORSRAM_TimingTypeDef FSMC_NORSRAMTimingInitStructureRead;
+    FSMC_NORSRAM_TimingTypeDef FSMC_NORSRAMTimingInitStructureWrite;
+    HAL_StatusTypeDef status = HAL_OK;
+
+    memset(&FSMC_NORSRAMInitStructure, 0, sizeof(FSMC_NORSRAMInitStructure));
+    memset(&FSMC_NORSRAMTimingInitStructureRead, 0, sizeof(FSMC_NORSRAMTimingInitStructureRead));
+    memset(&FSMC_NORSRAMTimingInitStructureWrite, 0, sizeof(FSMC_NORSRAMTimingInitStructureWrite));
+
+    /* Enable FSMC Clock */
+    __HAL_RCC_FSMC_CLK_ENABLE();
+
+    /* Define protocol type */
+    FSMC_NORSRAMInitStructure.NSBank = FSMC_NORSRAM_BANK1;
+    FSMC_NORSRAMInitStructure.DataAddressMux = FSMC_DATA_ADDRESS_MUX_DISABLE;
+    FSMC_NORSRAMInitStructure.MemoryType = FSMC_MEMORY_TYPE_SRAM;
+    FSMC_NORSRAMInitStructure.MemoryDataWidth = FSMC_NORSRAM_MEM_BUS_WIDTH_16;
+    FSMC_NORSRAMInitStructure.BurstAccessMode =  FSMC_BURST_ACCESS_MODE_DISABLE;
+    FSMC_NORSRAMInitStructure.AsynchronousWait = FSMC_ASYNCHRONOUS_WAIT_DISABLE;
+    FSMC_NORSRAMInitStructure.WaitSignalPolarity =  FSMC_WAIT_SIGNAL_POLARITY_LOW;
+    FSMC_NORSRAMInitStructure.WrapMode = FSMC_WRAP_MODE_DISABLE;
+    FSMC_NORSRAMInitStructure.WaitSignalActive = FSMC_WAIT_TIMING_BEFORE_WS;
+    FSMC_NORSRAMInitStructure.WriteOperation = FSMC_WRITE_OPERATION_ENABLE;
+    FSMC_NORSRAMInitStructure.WaitSignal = FSMC_WAIT_SIGNAL_DISABLE;
+    FSMC_NORSRAMInitStructure.ExtendedMode = FSMC_EXTENDED_MODE_ENABLE;
+    FSMC_NORSRAMInitStructure.WriteBurst = FSMC_WRITE_BURST_DISABLE;
+
+    status = FSMC_NORSRAM_Init(FSMC_Bank1, &FSMC_NORSRAMInitStructure);
+
+#if notyet
+    /* Define Read timing parameters */
+    FSMC_NORSRAMTimingInitStructureRead.AddressSetupTime = 2;
+    FSMC_NORSRAMTimingInitStructureRead.AddressHoldTime = 0;
+    FSMC_NORSRAMTimingInitStructureRead.DataSetupTime = 5;
+    FSMC_NORSRAMTimingInitStructureRead.BusTurnAroundDuration = 0;
+    FSMC_NORSRAMTimingInitStructureRead.CLKDivision =  0;
+    FSMC_NORSRAMTimingInitStructureRead.DataLatency = 0;
+    FSMC_NORSRAMTimingInitStructureRead.AccessMode = FSMC_ACCESS_MODE_B;
+#endif
+
+    /* Define Write Timing parameters */
+    FSMC_NORSRAMTimingInitStructureWrite.AddressSetupTime = 3;
+    FSMC_NORSRAMTimingInitStructureWrite.AddressHoldTime = 0;
+    FSMC_NORSRAMTimingInitStructureWrite.DataSetupTime = 6;
+    FSMC_NORSRAMTimingInitStructureWrite.BusTurnAroundDuration = 0;
+    FSMC_NORSRAMTimingInitStructureWrite.CLKDivision = 1;
+    FSMC_NORSRAMTimingInitStructureWrite.DataLatency = 0;
+    FSMC_NORSRAMTimingInitStructureWrite.AccessMode = FSMC_ACCESS_MODE_A;
+
+    // FSMC_NORSRAM_Init(FSMC_Bank1, &FSMC_NORSRAMInitStructure); // XXXOP ?
+
+    status = FSMC_NORSRAM_Timing_Init(FSMC_Bank1,  &FSMC_NORSRAMTimingInitStructureWrite, FSMC_NORSRAM_BANK1);
+
+    // Enable FSMC
+    __FSMC_NORSRAM_ENABLE(FSMC_Bank1, FSMC_NORSRAM_BANK1);
+
+    this->fsmc_cmd_baddr = (volatile uint16_t*)NOR_MEMORY_ADRESS1;
+    this->fsmc_data_baddr = (this->fsmc_cmd_baddr + (1<<18));
 }
 
 void
